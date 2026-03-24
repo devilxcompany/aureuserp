b87195b406b2f71c154bbd0e74f4b6a48ac92693
Michael Phong Do <devilxcompany@gmail.com>
2026-03-24 21:41:54 +1100

    Create Supabase PostgreSQL configuration file.

---
+++ database/config/supabase.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => 'your_supabase_host',
+    'database' => 'your_database_name',
+    'username' => 'your_username',
+    'password' => 'your_password',
+    'port' => '5432',
+];
